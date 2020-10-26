5b7cc553bb38b69a4e70ec2bd9038351d74caed6
Petros-Kontos <p_kontos@hotmail.com>
2020-10-26 11:27:02 +0200

    replace if statement

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const button = document.getElementById('button');
 const result = document.getElementById('result');
 const author = document.getElementById('author');
 const photo = document.getElementById('photo');
+const photos = {
+	'Walter White':'walter.png',
+	'Mike Ehrmantraut':'mike.jpg',
+	'Hank Schrader':'hank.jpg',
+	'Jesse Pinkman':'jesse.jpg',
+	'Saul Goodman':'saul.jpg',
+	'Gustavo Fring':'gus.jpg',
+	'Skyler White':'skyler.jpg',
+	'Badger':'badger.jpg',
+	'The fly':'fly.jpg',
+}
 
 button.addEventListener('click', getQuote);
 
@@ -11,48 +22,6 @@ function getQuote() {
 		.then(data => {
             result.innerHTML = data[0].quote
 			author.innerHTML = data[0].author
-			if (data[0].author == 'Walter White') {
-				photo.innerHTML = `<img src='img/walter.png'/>`
-			}
-			if (data[0].author == 'Mike Ehrmantraut') {
-				photo.innerHTML = `<img src='img/mike.jpg'/>`
-			}
-			if (data[0].author == 'Hank Schrader') {
-				photo.innerHTML = `<img src='img/hank.jpg'/>`
-			}
-			if (data[0].author == 'Jesse Pinkman') {
-				photo.innerHTML = `<img src='img/jesse.jpg'/>`
-			}
-			if (data[0].author == 'Saul Goodman') {
-				photo.innerHTML = `<img src='img/saul.jpg'/>`
-			}
-			if (data[0].author == 'Gustavo Fring') {
-				photo.innerHTML = `<img src='img/gus.jpg'/>`
-			}
-			if (data[0].author == 'Skyler White') {
-				photo.innerHTML = `<img src='img/skyler.jpg'/>`
-			}
-			if (data[0].author == 'Badger') {
-				photo.innerHTML = `<img src='img/badger.jpg'/>`
-			}
-			if (data[0].author == 'The fly') {
-				photo.innerHTML = `<img src='img/fly.jpg'/>`
-			}
+			photo.innerHTML = `<img src='img/${photos[data[0].author]}'/>`
 		});
 }
-
-
-
-
-
-
-
-/*
-function getQuote() {
-	fetch('https://aws.random.cat/meow')
-		.then(res => res.json())
-		.then(data => {
-			result.innerHTML = `<img src=${data.file} alt="cat" />`
-		});
-}
-*/
